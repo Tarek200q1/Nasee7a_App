a43804575f49b33cda7587c4165725b600731766
Tarek200q1 <tm211270@gmail.com>
2025-10-16 15:12:19 +0300

    feature: more vercal

--- src/index.js
+++ src/index.js
@@ -67,5 +67,5 @@ app.use((req , res)=>{
 
 //Running Server
 app.listen(process.env.PORT , ()=>{
-  console.log(`Server is running on port ` + process.env.PORT );
+  console.log(`Server is running on port ` + process.env.PORT);
 })
